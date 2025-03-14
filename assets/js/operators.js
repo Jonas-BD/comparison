f8bcded0a86cbd59654e71977ffe78eeb8ebfb57
Jonas-BD <joda12@elev.techcollege.dk>
2025-03-14 12:27:54 +0100

    small changes

--- assets/js/operators.js
+++ assets/js/operators.js
@@ -9,18 +9,14 @@ const F = undefined;
 
 // Opgave 1: skriv et expression der viser true når A er lig med B eller A er lig med D.
 
-if ( A === B || A === D) {
+if ( A == B || A == D) {
     console.log('statement true!');
-} else {
-    console.log('statement false!');
 }
 
 
 //opgave 2: skrive en If then statement der bliver TRUE når  A er større end B og A er mindre end E.
 if ( A > B && A < E) {
     console.log('statement true!');
-} else {
-    console.log('statement false!');
 }
 
 
@@ -28,16 +24,9 @@ if ( A > B && A < E) {
 //opgave 3: Skriv en If then statement med  && og  || der kun er sand når alle expressions er sande.
 if ( (A > B && A < E) || A === D) {
     console.log('statement true!');
-} else {
-    console.log('statement false!');
 }
 
-
-
-
 //opgave 4: Skriv en If then statement der kan finde ud af om F er "undefined" eller har en værdi, ved at bruge !.
-if ( F !== undefined) {
+if ( !F ) {
     console.log('statement true!');
-} else {
-    console.log('statement false!');
 }
